8fcc869cfd165062184813fdee94e35c3c92513a
jwijaya <jwijaya@binus.edu>
2014-05-14 14:57:28 +0700

    tekkom

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+var checkoperandtable = true;
 $(document).ready(function(){
 	//view
 	$('#next').click(function(){
@@ -16,6 +17,22 @@ $(document).ready(function(){
 			$('#back').removeClass('iTableManager').addClass('iStatement');
 			$('#next').removeClass('iTableManager').addClass('iStatement');
 		}
+		else if($(sv).hasClass('iStatement')){
+			checkoperandtable = true;
+			checkStatement();
+			if(checkoperandtable == false){
+				alert('Operand tersebut belum terdaftar di dalam table');
+			}
+			else{
+				$('#iStatement').css('display','none');
+				$('#iTree').css('display','');
+				
+				$('#back').removeClass('iStatement').addClass('iTree');
+				$('#next').removeClass('iStatement').addClass('iTree');
+				
+				statement();
+			}
+		}
 	});
 	$('#back').click(function(){
 		var sv=this;
@@ -33,31 +50,114 @@ $(document).ready(function(){
 			$('#back').removeClass('iStatement').addClass('iTableManager');
 			$('#next').removeClass('iStatement').addClass('iTableManager');
 		}
+		else if($(sv).hasClass('iTree')){
+			$('#iTree').css('display','none');
+			$('#iStatement').css('display','');
+			
+			$('#back').removeClass('iTree').addClass('iStatement');
+			$('#next').removeClass('iTree').addClass('iStatement');
+		}
 	});
 	
 	//table manager
 	var tempdatatype={
-		value: ["int","float","token","token"],
-		desc: ["Integer","Float","String","Char"]
+		value: ["int","float","token","token","token"],
+		desc: ["Integer","Float","Varchar","Char","Boolean"]
 	};
 	var content="";
 	for(var i = 1 ; i < 3 ; i++){
-		content='<tr><td><span id="lexeme-id">'+i
-			+'</span></td><td><input type="text" id="lexeme" class="form-control"/></td><td><select class="form-control">';
+		content='<tr><td><span class="lexeme-id">'+i
+			+'</span></td><td><input type="text" class="form-control lexeme"/></td><td><select class="form-control dataType">';
 			for(var j = 0 ; j < 4 ; j++)
 				content+='<option value="'+tempdatatype.value[j]+'">'+tempdatatype.desc[j]+'</option>';
 		content+='</select></td></tr>';
 		$('#table-manager').append(content);
 	}
 	$('#button-add').click(function(){
-		content='<tr><td><span id="lexeme-id">'+($('#table-manager tbody tr').length+1)
-		+'</span></td><td><input type="text" id="lexeme" class="form-control"/></td><td><select class="form-control">';
+		content='<tr><td><span class="lexeme-id">'+($('#table-manager tbody tr').length+1)
+			+'</span></td><td><input type="text" class="form-control lexeme"/></td><td><select class="form-control dataType">';
 			for(var j = 0 ; j < 4 ; j++)
 				content+='<option value="'+tempdatatype.value[j]+'">'+tempdatatype.desc[j]+'</option>';
 		content+='</select></td></tr>';
 		$('#table-manager').append(content);
 	});
 	
-	//statement
 	
 });
+
+
+//statement
+function statement(){
+	txt = $('#statement').val();
+	var txt,tbl,
+		listR = ["=","*","/","+","-"];
+}
+
+//check statement
+function checkStatement(){
+	var s = $('#statement').val();
+	var operator = [],operand = [],t,n,
+		listR = ["=","*","/","+","-"];
+	var sLen = s.length;
+	if(s)
+	{	
+		for(var i = 0; i < sLen; i++)
+		{
+			t = s[i];
+			if(t === ' ') continue;
+			var j = i + 1;
+			if(listR.indexOf(s[i]) !== -1)
+			{
+				for(var j = i + 1;listR.indexOf(s[j]) !== -1; j++)
+				{
+					t += s[j];
+				}
+				i += t.length - 1;
+				if(listR.indexOf(t) !== -1)
+				{
+					operator.push(t);
+				}
+				else 
+				{
+					error = true;
+					alert('error');
+				}
+			}
+			else if(s[i] !== ' ')
+			{
+				for(var j = i + 1; s[j] && s[j] !== ' ' && listR.indexOf(s[j]) === -1;j++)
+				{
+					t += s[j];
+				}
+				i += t.length -1;
+				
+				operand.push(t);
+				
+			}
+		}			
+	}
+	
+	var tr = $('#table-manager tbody tr');
+	var checkoperand = 0;
+	for(var i = 0 ; i < operand.length ; i++){
+		if (operand.indexOf(i) === -1)
+			continue;
+		else{
+			while($(tr).length !== 0)
+			{
+				if($('.lexeme',tr).val() == operand[i]){
+					checkoperand++;
+					break;
+				}
+				tr = $(tr).next();
+			}
+		}
+	}
+	console.log(operand);
+	console.log(operator);
+	
+	if(checkoperand != operand.length)
+		checkoperandtable = false;
+		
+	
+}
