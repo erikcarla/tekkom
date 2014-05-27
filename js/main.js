f5cdba7045869918f97730abb0fa8abb0d7d8a77
Erik Carla <erikcarla33@gmail.com>
2014-05-27 23:27:16 +0700

    tekkom

--- js/main.js
+++ js/main.js
@@ -34,18 +34,36 @@ $(document).ready(function(){
 		else if($(sv).hasClass('iStatement')){
 			checkoperandtable = true;
 			checkStatement();
-			// if(checkoperandtable == false){
-				// alert('Operand tersebut belum terdaftar di dalam table');
-			// }
-			// else{
+			if(checkoperandtable == false){
+				alert('Operand tersebut belum terdaftar di dalam table');
+			}
+			else if(operator[0] == undefined || operand[0] == undefined || operand[1] == undefined )
+				alert('Error');
+			else if($('#iTokenText').val() == '')
+				alert('Token belum terdaftar');
+			else{
 				$('#iStatement').css('display','none');
-				$('#iTree').css('display','');
+				$('#iLexical').css('display','');
 				
-				$('#back').removeClass('iStatement').addClass('iTree');
-				$('#next').removeClass('iStatement').addClass('iTree');
+				$('#back').removeClass('iStatement').addClass('iLexical');
+				$('#next').removeClass('iStatement').addClass('iLexical');
 				
 				statement();
-			// }
+			}
+		}
+		else if($(sv).hasClass('iLexical')){
+			$('#iLexical').css('display','none');
+			$('#iTree').css('display','');
+			
+			$('#back').removeClass('iLexical').addClass('iTree');
+			$('#next').removeClass('iLexical').addClass('iTree');
+		}
+		else if($(sv).hasClass('iTree')){
+			$('#iTree').css('display','none');
+			$('#iSemantic').css('display','');
+			
+			$('#back').removeClass('iTree').addClass('iSemantic');
+			$('#next').removeClass('iTree').addClass('iSemantic');
 		}
 	});
 	$('#back').click(function(){
@@ -78,12 +96,26 @@ $(document).ready(function(){
 			$('#back').removeClass('iStatement').addClass('iTableManager');
 			$('#next').removeClass('iStatement').addClass('iTableManager');
 		}
-		else if($(sv).hasClass('iTree')){
-			$('#iTree').css('display','none');
+		else if($(sv).hasClass('iLexical')){
+			$('#iLexical').css('display','none');
 			$('#iStatement').css('display','');
 			
-			$('#back').removeClass('iTree').addClass('iStatement');
-			$('#next').removeClass('iTree').addClass('iStatement');
+			$('#back').removeClass('iLexical').addClass('iStatement');
+			$('#next').removeClass('iLexical').addClass('iStatement');
+		}
+		else if($(sv).hasClass('iTree')){
+			$('#iTree').css('display','none');
+			$('#iLexical').css('display','');
+			
+			$('#back').removeClass('iTree').addClass('iLexical');
+			$('#next').removeClass('iTree').addClass('iLexical');
+		}
+		else if($(sv).hasClass('iSemantic')){
+			$('#iSemantic').css('display','none');
+			$('#iTree').css('display','');
+			
+			$('#back').removeClass('iSemantic').addClass('iTree');
+			$('#next').removeClass('iSemantic').addClass('iTree');
 		}
 	});
 	
@@ -133,27 +165,33 @@ function statement(){
 	var tempseqoperator=[],tempseqtoken=[],token =[], datatype =[],
 		sequence = ["^","%","*","/","+","-","="];
 	var sLen = s.length;
+	var ceknumber = true;
 	//token
 	for(var i = 0 ; i < operand.length ; i++){
-		if($.isNumeric(operand[i]))
-			token.push(operand[i]);
-		else{
-			var tr = $('#table-manager tbody tr').first();
-			while($(tr).length !== 0)
-			{
-				if($('.lexeme',tr).val() == operand[i]){
-					token.push('&lt;'+ $('#iTokenText').val()+','+ $('.lexeme-id',tr).text()+'&gt;');
-					datatype.push($('.dataType',tr).text());
-					break;
-				}
-				tr = $(tr).next();
+		var tr = $('#table-manager tbody tr').first();
+		while($(tr).length !== 0)
+		{
+			if($('.lexeme',tr).val() == operand[i]){
+				token.push('&lt;'+ $('#iTokenText').val()+','+ $('.lexeme-id',tr).text()+'&gt;');
+				if($.isNumeric(operand[i]))
+					ceknumber = false;
+				break;
 			}
+			tr = $(tr).next();
 		}
+		if($.isNumeric(operand[i]) && ceknumber == true)
+			token.push(operand[i]);
 	}
 	//want to get token? print token.. want to get datatype? print datatype	
 	
+	for(var j = 0 ; j < token.length ; j++){
+		if(j == token.length-1)
+			$('#lex').append(token[j]);
+		else
+			$('#lex').append(token[j]+' '+operator[j]+' ');
+	}
 	
-	//penampungan ke sebuah object untuk dibuat tree
+	//penampungan ke sebuah object untuk dibuat syntax`
 	var state = {};
 	var x = 0;
 	var kurung = 0;
@@ -161,6 +199,7 @@ function statement(){
 	
 	tempseqoperator = operator;
 	tempseqtoken = token;
+	
 	
 	for(var j = 0 ; j < tempseqoperator.length ; j++){
 		if(tempseqoperator[j] == '('){
@@ -251,59 +290,167 @@ function statement(){
 	var text = '';
 	text+='<ul>'
 		text+='<li>';
-			text+='<a href="#">'+state['obj'+(x-1)].parent.toString()+'</a>';
+			text+='<a href="#">'+state['obj'+(x-1)].parent+'</a>';
 			text+='<ul>';
 				text+='<li>';
-					if(state['obj'+(x-1)].left.indexOf('obj') == 0){
-						text+=printtree(state['obj'+(x-1)].left,state);
-					}else{
-						text+='<a href="#">';
-							text+=state['obj'+(x-1)].left.toString();
-						text+='</a>';
-					}
+				if(state['obj'+(x-1)].left.indexOf('obj') == 0){
+					text+=printtree(state['obj'+(x-1)].left,state);
+				}else{
+					text+='<a href="#">';
+						text+=state['obj'+(x-1)].left;
+					text+='</a>';
+				}
 				text+='</li>';
 				text+='<li>';
 				if(state['obj'+(x-1)].right.indexOf('obj') == 0){
 					text+=printtree(state['obj'+(x-1)].right,state);
 				}else{
 					text+='<a href="#">';
-						text+=state['obj'+(x-1)].right.toString();
+						text+=state['obj'+(x-1)].right;
 					text+='</a>';
 				}
 				text+='</li>';
 			text+='</ul>';
 		text+='</li>';
 	text+='</ul>';
-	$('.tree').append(text);
+	$('#syntax').append(text);
 	
-	$('.tree').css('width',$('.tree ul').length * 80);
-	console.log(state);
+	$('#syntax').css('width',$('#syntax ul').length * 80);
+	
+	//semantic 
+	
+	checkStatement();
+	token = [];
+	//token
+	for(var i = 0 ; i < operand.length ; i++){
+		var tr = $('#table-manager tbody tr').first();
+		while($(tr).length !== 0)
+		{
+			if($('.lexeme',tr).val() == operand[i]){
+				token.push('&lt;'+ $('#iTokenText').val()+','+ $('.lexeme-id',tr).text()+'&gt;');
+				datatype.push($('.dataType',tr).val());
+				if($.isNumeric(operand[i]))
+					ceknumber = false;
+				break;
+			}
+			tr = $(tr).next();
+		}
+		if($.isNumeric(operand[i]) && ceknumber == true){
+			token.push(operand[i]);
+			datatype.push('float');
+		}
+	}
+	
+	state = {};
+	x = 0;
+	kurung = 0;
+	temporar = 0;
+	var tempsemoperator = operator;
+	var tempsemtoken = token;
+	for(var j = 0 ; j < datatype.length ; j++){
+		if($('.dataType').val() != datatype[j] && datatype[j] != 'token'){
+			var tree = {'left':tempsemtoken[j],'right':null,'parent':datatype[j]+' to '+$('.dataType').val()};
+			state['obj'+x]=tree;
+			tempsemtoken.splice(j,1,'obj'+x);
+			x++;
+		}
+	}
+	
+	for(var j = 0 ; j < tempsemtoken.length ; j++){
+		if(tempsemoperator[j] == '/' || tempsemoperator[j] == '*' || tempsemoperator[j] == '%'){
+			var tree = {'left':tempsemtoken[j],'right':tempsemtoken[j+1],'parent':tempsemoperator[j]};
+			state['obj'+x]=tree;
+			tempsemtoken.splice(j,2,'obj'+x);
+			tempsemoperator.splice(j,1);
+			x++;
+			j = -1;
+		}	
+	}
+	for(var j = 0 ; j < tempsemtoken.length ; j++){
+		if(tempsemoperator[j] == '+' || tempsemoperator[j] == '-'){
+			var tree = {'left':tempsemtoken[j],'right':tempsemtoken[j+1],'parent':tempsemoperator[j]};
+			state['obj'+x]=tree;
+			tempsemtoken.splice(j,2,'obj'+x);
+			tempsemoperator.splice(j,1);
+			x++;
+			j = -1;
+		}	
+	}
+	
+	for(var j = 0 ; j < tempsemtoken.length ; j++){
+		if(tempsemoperator[j] == '='){
+			var tree = {'left':tempsemtoken[j],'right':tempsemtoken[j+1],'parent':tempsemoperator[j]};
+			state['obj'+x]=tree;
+			tempsemtoken.splice(j,2,'obj'+x);
+			tempsemoperator.splice(j,1);
+			x++;
+			j = -1;
+		}	
+	}
+	
+	text = '';
+	text+='<ul>'
+		text+='<li>';
+			text+='<a href="#">'+state['obj'+(x-1)].parent+'</a>';
+			text+='<ul>';
+				if(state['obj'+(x-1)].left != null){
+					text+='<li>';
+					if(state['obj'+(x-1)].left.indexOf('obj') == 0){
+						text+=printtree(state['obj'+(x-1)].left,state);
+					}else{
+						text+='<a href="#">';
+							text+=state['obj'+(x-1)].left;
+						text+='</a>';
+					}
+					text+='</li>';
+				}
+				if(state['obj'+(x-1)].right !== null){
+					text+='<li>';
+					if(state['obj'+(x-1)].right.indexOf('obj') == 0){
+						text+=printtree(state['obj'+(x-1)].right,state);
+					}else{
+						text+='<a href="#">';
+							text+=state['obj'+(x-1)].right;
+						text+='</a>';
+					}
+					text+='</li>';
+				}
+			text+='</ul>';
+		text+='</li>';
+	text+='</ul>';
+	$('#semantic').append(text);
+	
+	$('#semantic').css('width',$('#syntax ul').length * 80);
 }
 
 
 //tree
 function printtree(obj,state){
 	var text = '';
-	text+='<a href="#">'+state[obj].parent.toString()+'</a>';
+	text+='<a href="#">'+state[obj].parent+'</a>';
 	text+='<ul>';
-		text+='<li>';
-		if(state[obj].left.indexOf('obj') == 0){
-			text+=printtree(state[obj].left,state);
-		}else{
-			text+='<a href="#">';
-				text+=state[obj].left.toString();
-			text+='</a>';
+		if(state[obj].left != null){
+			text+='<li>';
+			if(state[obj].left.indexOf('obj') == 0){
+				text+=printtree(state[obj].left,state);
+			}else{
+				text+='<a href="#">';
+					text+=state[obj].left;
+				text+='</a>';
+			}
+			text+='</li>';
 		}
-		text+='</li>';
-		text+='<li>';
-		if(state[obj].right.indexOf('obj') == 0){
-			text+=printtree(state[obj].right,state);
-		}else{
-			text+='<a href="#">';
-				text+=state[obj].right.toString();
-			text+='</a>';
+		if(state[obj].right != null){
+			text+='<li>';
+			if(state[obj].right.indexOf('obj') == 0){
+				text+=printtree(state[obj].right,state);
+			}else{
+				text+='<a href="#">';
+					text+=state[obj].right;
+				text+='</a>';
+			}
+			text+='</li>';
 		}
-		text+='</li>';
 	text+='</ul>';
 	return text;
 }
@@ -339,17 +486,18 @@ function checkStatement(){
 					t += s[j];
 				}
 				i += t.length -1;
-				if (operand.indexOf(t) === -1 /*&& $.isNumeric(operand)*/)
-					tempop.push(t);
+				if($.isNumeric(t) == false)
+					if (operand.indexOf(t) === -1)
+						tempop.push(t);
 				operand.push(t);
 				
 			}
 		}			
 	}
 	
-	var tr = $('#table-manager tbody tr');
 	var checkoperand = 0;
 	for(var i = 0 ; i < tempop.length ; i++){
+		var tr = $('#table-manager tbody tr');
 		while($(tr).length !== 0)
 		{
 			if($('.lexeme',tr).val() == tempop[i]){
@@ -362,7 +510,6 @@ function checkStatement(){
 	
 	if(checkoperand != tempop.length)
 		checkoperandtable = false;
-		
 }
 
 
